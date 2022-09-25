69404f5a0286a2f3aee5a52c184ddb0a088f06a1
Jake Cronin <jcronin97@live.ca>
2022-09-25 12:18:05 -0700

    Added bootloader folder, reference files

---
+++ boot/src/startup.asm
@@ -0,0 +1,13 @@
+.global _stextload
+.global _stext
+.global _etext
+.global _sdataload
+.global _sdata
+.global _edata
+.global _sbss
+.global _ebss
+.global _flexram_bank_config
+.global _estack
+
+startup:
+    
